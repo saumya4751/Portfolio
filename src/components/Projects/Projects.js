b08fad1790431444782fda6e084d730dc4ccebed
Saumya Shah <saumyashah4750@gmail.com>
2024-03-20 17:25:20 -0400

    .

---
+++ src/components/Projects/Projects.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Container, Desc, Title, Wrapper } from '../Skills/SkillsStyledComponents'
+
+const Projects = () => {
+  return (
+    <Container id="projects">
+        <Wrapper>
+        <Title>Education</Title>
+            <Desc>
+                My education has been a journey of self-discovery and growth. My educational details are as follows.
+            </Desc>
+        </Wrapper>
+    </Container>
+  )
+}
+
+export default Projects
