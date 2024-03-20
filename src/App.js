845918bd0c8148c3be856fd32f45e0cd03076699
Saumya Shah <saumyashah4750@gmail.com>
2024-03-19 20:30:08 -0400

    navbar completed

--- src/App.js
+++ src/App.js
@@ -1,9 +1,36 @@
 import './App.css';
+import styled, { ThemeProvider } from "styled-components";
+import {  BrowserRouter as Router } from 'react-router-dom';
+import { darkTheme, lightTheme } from './utils/Themes';
+import Navbar from './components/NavBar/NavBar';
+
+const Body = styled.div`
+  background-color: ${({ theme }) => theme.bg};
+  width: 100%;
+  height: 100%;
+  overflow-x: hidden;
+`;
+
+const Wrapper = styled.div`
+  background:linear-gradient(38.73deg, rgba(204, 0, 187, 0.15) 0%, rgba(201, 32, 184, 0) 50%), linear-gradient(141.27deg, rgba(0, 70, 209, 0) 50%, rgba(0, 70, 209, 0.15) 100%);
+  width: 100%;
+  clip-path: polygon(0 0, 100% 0, 100% 100%,30% 98%, 0 100%);
+`;
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello, World!</h1>
+      <ThemeProvider theme={lightTheme} >
+        <Router>
+          <Navbar />
+          <Body>
+            {/* <About />
+            <Skills />
+            <Experience />
+            <Education />             */}
+          </Body>
+        </Router>
+      </ThemeProvider>
     </div>
   );
 }
