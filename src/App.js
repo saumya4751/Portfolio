fbcb01114c2a0e7a9dfd67bff63551dbebcb240a
Saumya Shah <saumyashah4750@gmail.com>
2024-03-19 22:47:07 -0400

    skills section completed

--- src/App.js
+++ src/App.js
@@ -2,7 +2,9 @@ import './App.css';
 import styled, { ThemeProvider } from "styled-components";
 import {  BrowserRouter as Router } from 'react-router-dom';
 import { darkTheme, lightTheme } from './utils/Themes';
+import { motion, useScroll } from "framer-motion";
 import Navbar from './components/NavBar/NavBar';
+import Skills from './components/Skills/Skills';
 import Home from './components/Home/Home';
 
 const Body = styled.div`
@@ -15,7 +17,7 @@ const Body = styled.div`
 const Wrapper = styled.div`
   background:linear-gradient(38.73deg, rgba(204, 0, 187, 0.15) 0%, rgba(201, 32, 184, 0) 50%), linear-gradient(141.27deg, rgba(0, 70, 209, 0) 50%, rgba(0, 70, 209, 0.15) 100%);
   width: 100%;
-  clip-path: polygon(0 0, 100% 0, 100% 100%,30% 98%, 0 100%);
+  /* clip-path: polygon(0 0%, 100% 0, 100% 100%,30% 98%, 0 100%); */
 `;
 
 function App() {
@@ -26,9 +28,11 @@ function App() {
           <Navbar />
           <Body>
             <Home />
-            {/* <Skills />
-            <Experience />
-            <Education />             */}
+            <Wrapper>
+              <Skills />
+              {/* <Experience />
+              <Education />             */}
+            </Wrapper>
           </Body>
         </Router>
       </ThemeProvider>
