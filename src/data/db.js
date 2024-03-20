139b5268b170e9feeb3471c2b1bc96d5f771b53f
Saumya Shah <saumyashah4750@gmail.com>
2024-03-19 20:15:17 -0400

    db file added

---
+++ src/data/db.js
@@ -0,0 +1,17 @@
+export const Bio = {
+    name: "Saumya Shah",
+    roles: [
+      "Full Stack Engineer",
+      "Frontend Engineer",
+      "Backend Engineer",
+    ],
+    description:
+      "A results-driven Full Stack Engineer with a master’s degree in computer science, specializes in developing scalable web applications, optimizing user experiences, and implementing complex algorithms. Proficient in a wide range of technologies including Python, Java, JavaScript, and various databases. Passionate about leveraging technical skills to solve complex problems and drive business success.",
+    github: "https://github.com/saumya4751",
+    resume:
+      "https://drive.google.com/file/d/1Bt6pZ9E2s_b0Ec_HHS-1Cgdk2FRst2BB/view?usp=sharing",
+    linkedin: "https://www.linkedin.com/in/saumya4751/",
+    twitter: "https://twitter.com/RishavChanda",
+    insta: "https://www.instagram.com/rishav_chanda/",
+    facebook: "https://www.facebook.com/rishav.chanda.165/",
+  };
