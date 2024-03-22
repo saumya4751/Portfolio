d8e1de45ffdfe4cbe9c6c16bc91e27c8ea9997f6
Saumya Shah <saumyashah4750@gmail.com>
2024-03-21 20:05:25 -0400

    projects-added

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,17 +1,182 @@
-import React from 'react'
-import { Container, Desc, Title, Wrapper } from '../Skills/SkillsStyledComponents'
+// import { Container, Desc, Title, Wrapper } from '../Skills/SkillsStyledComponents'
 
-const Projects = () => {
+import React, { useState } from "react";
+import styled from "styled-components";
+import { projects } from "../../data/db";
+import ProjectCard from "./ProjectsCard";
+
+const Container = styled.div`
+  margin-top: 100px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  position: relative;
+  z-index: 1;
+  padding: 0 16px;
+  align-items: center;
+`;
+const Wrapper = styled.div`
+  position: relative;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  flex-direction: column;
+  width: 100%;
+  max-width: 1100px;
+  gap: 12px;
+  @media (max-width: 960px) {
+    flex-direction: column;
+  }
+`;
+
+const Title = styled.div`
+  font-size: 52px;
+  text-align: center;
+  font-weight: 600;
+  margin-top: 80px;
+  color: ${({ theme }) => theme.text_primary};
+  @media (max-width: 768px) {
+    margin-top: 12px;
+    font-size: 32px;
+  }
+`;
+
+const Desc = styled.div`
+  font-size: 18px;
+  text-align: center;
+  font-weight: 600;
+  color: ${({ theme }) => theme.text_secondary};
+  @media (max-width: 768px) {
+    font-size: 16px;
+  }
+`;
+
+const ToggleButtonGroup = styled.div`
+  display: flex;
+  border: 1.5px solid ${({ theme }) => theme.primary};
+  color: ${({ theme }) => theme.primary};
+  font-size: 16px;
+  border-radius: 12px;
+  font-weight: 500;
+  margin: 10px 0;
+  margin-bottom: 5%;
+  @media (max-width: 768px){
+      font-size: 12px;
+  }
+`;
+
+const ToggleButton = styled.div`
+  padding: 5px 20px;
+  border-radius: 6px;
+  cursor: pointer;
+  &:hover {
+    background: ${({ theme }) => theme.primary + 20};
+  }
+  @media (max-width: 768px) {
+    padding: 6px 8px;
+    border-radius: 4px;
+  }
+  ${({ active, theme }) =>
+    active &&
+    `
+  background:  ${theme.primary + 20};
+  `}
+`;
+
+const Divider = styled.div`
+  width: 1.5px;
+  background: ${({ theme }) => theme.primary};
+`;
+
+const CardContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 30px;
+  flex-wrap: wrap;
+`;
+
+const Projects = ({ openModal, setOpenModal }) => {
+  const [toggle, setToggle] = useState("all");
   return (
     <Container id="projects">
-        <Wrapper>
-        <Title>Education</Title>
-            <Desc>
-                My education has been a journey of self-discovery and growth. My educational details are as follows.
-            </Desc>
-        </Wrapper>
+      <Wrapper>
+        <Title>Projects</Title>
+        <Desc
+          style={{
+            marginBottom: "20px",
+          }}
+        >
+          I have worked on a wide range of projects. From web apps to android
+          apps. Here are some of my projects.
+        </Desc>
+        {/* <ToggleButtonGroup>
+          <ToggleButton
+            active={toggle === "all"}
+            onClick={() => setToggle("all")}
+          >
+            ALL
+          </ToggleButton>
+          <Divider />
+          <ToggleButton
+            active={toggle === "web app"}
+            onClick={() => setToggle("web app")}
+          >
+            WEB APP"S
+          </ToggleButton>
+          <Divider />
+          <ToggleButton
+            active={toggle === "android app"}
+            onClick={() => setToggle("android app")}
+          >
+            ANDROID APP'S
+          </ToggleButton>
+          <Divider />
+          <ToggleButton
+            active={toggle === "machine learning"}
+            onClick={() => setToggle("machine learning")}
+          >
+            MACHINE LEARNING
+          </ToggleButton>
+        </ToggleButtonGroup> */}
+        <CardContainer>
+          {toggle === "all" &&
+            projects.map((project) => (
+              <ProjectCard
+                project={project}
+                openModal={openModal}
+                setOpenModal={setOpenModal}
+              />
+            ))}
+          {projects
+            .filter((item) => item.category === toggle)
+            .map((project) => (
+              <ProjectCard
+                project={project}
+                openModal={openModal}
+                setOpenModal={setOpenModal}
+              />
+            ))}
+        </CardContainer>
+      </Wrapper>
     </Container>
-  )
-}
+  );
+};
 
-export default Projects
+export default Projects;
+
+
+// const Projects = () => {
+//   return (
+//     <Container id="projects">
+//         <Wrapper>
+//         <Title>Education</Title>
+//             <Desc>
+//                 My education has been a journey of self-discovery and growth. My educational details are as follows.
+//             </Desc>
+//         </Wrapper>
+//     </Container>
+//   )
+// }
+
+// export default Projects
